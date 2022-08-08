4699b3ec131a13d3915d8392d86c6430ebdd789f
zhaozhuo <2424318315@qq.com>
2022-08-08 17:56:17 +0800

    项目初始化-调整初始目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!-- 路由的出口 -->
+    <router-view />
     <h1>头条</h1>
+    <div>
+      <i class="toutiao toutiao-shipin"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 <script>
@@ -10,5 +19,4 @@ export default {
 }
 </script>
 <style lang="less">
-
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/style/index.less'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 Vue.config.productionTip = false
 
+Vue.use(Vant)
 new Vue({
   router,
   store,
